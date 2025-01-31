dd66e95ff8e79b7e202ac8f8c9972a6386965b8d
wmwang <isosoman@gmail.com>
2025-02-01 02:44:28 +0800

    Update core.py

--- facefusion/core.py
+++ facefusion/core.py
@@ -390,8 +390,8 @@ def process_image(start_time : float) -> ErrorCode:
 
 def process_video(start_time : float) -> ErrorCode:
 	trim_frame_start, trim_frame_end = restrict_trim_frame(state_manager.get_item('target_path'), state_manager.get_item('trim_frame_start'), state_manager.get_item('trim_frame_end'))
-	if analyse_video(state_manager.get_item('target_path'), trim_frame_start, trim_frame_end):
-		return 3
+	#if analyse_video(state_manager.get_item('target_path'), trim_frame_start, trim_frame_end):
+	#	return 3
 	# clear temp
 	logger.debug(wording.get('clearing_temp'), __name__)
 	clear_temp_directory(state_manager.get_item('target_path'))
